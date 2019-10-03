444b25f925c5fcae4f1aa2592fae5c94f94376e5
Shivani <shivanipatel@Shivanis-MacBook-Pro-2.local>
2019-10-03 13:44:29 -0700

    Testing Dev branch

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const App = () => {
         <div class Name="ui container comments" >
             <CommentDetail author="Jane" timeAgo="Today at 4:45PM" commentContent="This a  good post" avatar={faker.image.avatar()}/>
             <CommentDetail author="Sam" timeAgo="Today at 2:00PM" commentContent="I dont like this so much" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Yesterday at 5:00PM" commentContent="SUperb blog!" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Abb" timeAgo="Yesterday at 5:00PM" commentContent="SUperb blog!" avatar={faker.image.avatar()}/>
         </div>
     );
 };
