7c523a64796a057d0dcbff7886014072b41f1d64
Shivani <shivanipatel@Shivanis-MacBook-Pro-2.local>
2019-10-03 13:25:40 -0700

    Very First Commit of Components

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+
+    return (
+        <div class Name="ui container comments" >
+            <CommentDetail author="Jane" timeAgo="Today at 4:45PM" commentContent="This a  good post" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Sam" timeAgo="Today at 2:00PM" commentContent="I dont like this so much" avatar={faker.image.avatar()}/>
+            <CommentDetail author="ABBC" timeAgo="Yesterday at 5:00PM" commentContent="SUperb blog!" avatar={faker.image.avatar()}/>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
